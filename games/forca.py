ed183a8441449ef678b846865a463490665768fb
melissapatez <m.patez@teltecsolutions.com.br>
2021-02-16 08:40:29 -0300

    Criando loop do jogo

--- games/forca.py
+++ games/forca.py
@@ -1,8 +1,16 @@
 
 def jogar_forca():
-    print("********************************")
-    print("Bem vindo no jogo da FORCA!")
-    print("********************************")
+    print("*********************************")
+    print("***Bem vindo ao jogo da FORCA!***")
+    print("*********************************")
+
+    palavra_secreta = "cinderella"
+    acertou = False
+    enforcou = False
+
+    while(not enforcou and not acertou):
+        
+
 
 if(__name__ "__main__"):
     jogar_forca()
