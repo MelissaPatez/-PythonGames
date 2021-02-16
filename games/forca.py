9c15e51f3305db6e45568d9c8999f28b9dd8b522
melissapatez <m.patez@teltecsolutions.com.br>
2021-02-16 11:48:01 -0300

    Criando funcao para gerar a palavra secreta

--- games/forca.py
+++ games/forca.py
@@ -1,10 +1,24 @@
+import random
 
 def jogar_forca():
     print("*********************************")
     print("***Bem vindo ao jogo da FORCA!***")
     print("*********************************")
 
-    palavra_secreta = "cinderella".upper()
+
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
+   
     letras_acertadas = ["_" for letra in palavra_secreta]
     
     acertou = False
